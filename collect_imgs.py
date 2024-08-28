1d4cbe8608fcaf7a251a8e726e78b81c70447852
Salim Alharthi <salim.h.harthi@gmail.com>
2024-08-28 13:40:38 +0400

    removed data file and adjusted data collection to collect and close

--- collect_imgs.py
+++ collect_imgs.py
@@ -9,7 +9,7 @@ if not os.path.exists(DATA_DIR):
 on = True
 dataset_size = 100
 j = 0
-cap = cv2.VideoCapture(2)
+cap = cv2.VideoCapture(0)
 while on:
     if not os.path.exists(os.path.join(DATA_DIR, str(j))):
         os.makedirs(os.path.join(DATA_DIR, str(j)))
@@ -19,21 +19,26 @@ while on:
     done = False
     while True:
         ret, frame = cap.read()
-        cv2.putText(frame, 'Ready? Press "R" ! :)', (100, 50), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+        cv2.putText(frame, 'To collect press "R" or to quit press "Q"', (20, 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1,
                     cv2.LINE_AA)
-        
-        cv2.putText(frame, 'To Quit Press "Q" ! :)', (100, 40), cv2.FONT_HERSHEY_SIMPLEX, 1.3, (0, 255, 0), 3,
+        cv2.putText(frame, f'Class Number: {j}', (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1,
                     cv2.LINE_AA)
+
         cv2.imshow('frame', frame)
         if cv2.waitKey(25) == ord('r'):
             break
-        if cv2.waitKey(25) == ord('q'):
+
+        elif cv2.waitKey(25) == ord('q'):
             on=False
             break
 
+    if not on:
+        break       
     counter = 0
     while counter < dataset_size:
         ret, frame = cap.read()
+        cv2.putText(frame, f'Class Number: {j}', (20, 75), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 0), 1,
+                    cv2.LINE_AA)
         cv2.imshow('frame', frame)
         cv2.waitKey(25)
         cv2.imwrite(os.path.join(DATA_DIR, str(j), '{}.jpg'.format(counter)), frame)
